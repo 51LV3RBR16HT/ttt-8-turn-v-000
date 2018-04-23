1181c9118d134a54b845a771091a1b819d4bcec9
Barbara Washington <barbara.m.washington@gmail.com>
2018-04-23 16:49:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,12 @@
 def turn(board)
   puts "Please enter 1-9:"
 end
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
